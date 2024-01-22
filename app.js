476ce9cbb640566ac48a91bae00fd9f6222278f2
Prasad More <prasadmoremain@gmail.com>
2024-01-22 17:27:26 +0530

    Added Data into a Table

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let showBtn = document.getElementById("showBtn")
 showBtn.addEventListener("click",showBooks)
 let booksArr = []
+let table = document.querySelector('table')
 
 function showBooks(){
 
     let books = new XMLHttpRequest();
+
 
     books.open("get",'books.json',true)
 
@@ -23,7 +25,18 @@ function showBooks(){
 
     }
 
-    console.log(booksArr)
+    display()
+    // console.log(booksArr)
 
 
+}
+
+
+function display(){
+    table.innerHTML+="<tr><th>Title</th><th>Author</th><th>Country</th><th>Language</th><th>Pages</th></tr>"
+
+    for(let i=0;i<20;i++){
+
+        table.innerHTML += "<tr><td>"+ booksArr[i].title +"</td><td>"+booksArr[i].author+"</td><td>"+booksArr[i].country+"</td><td>"+booksArr[i].language+"</td><td>"+booksArr[i].pages+"</td></tr>"
+    }
 }
