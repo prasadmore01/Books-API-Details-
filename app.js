946b526e6b5454cdbc8712f1ccf3609ffdb138ff
Prasad More <prasadmoremain@gmail.com>
2024-01-22 18:17:51 +0530

    Completed!

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ function showBooks(){
         let response = (this.responseText)
         let res = JSON.parse(response)
         
-        for(let i=0;i<50;i++){
+        for(let i=0;i<res.length;i++){
             booksArr.push(res[i])
         }
-    
+        display()
         }
 
     }
 
-    display()
+   
 
 
 
@@ -33,10 +33,10 @@ function showBooks(){
 
 
 function display(){
-    table.innerHTML+="<tr><th>Title</th><th>Author</th><th>Country</th><th>Language</th><th>Pages</th></tr>"
+    table.innerHTML+="<tr><th>Id</th><th>Title</th><th>Author</th><th>Country</th><th>Language</th><th>Pages</th></tr>"
 
-    for(let i=0;i<20;i++){
+    for(let i=0;i<50;i++){
 
-        table.innerHTML += "<tr><td>"+ booksArr[i].title +"</td><td>"+booksArr[i].author+"</td><td>"+booksArr[i].country+"</td><td>"+booksArr[i].language+"</td><td>"+booksArr[i].pages+"</td></tr>"
+        table.innerHTML += "<tr><td>"+(i+1)+"<td>"+ booksArr[i].title +"</td><td>"+booksArr[i].author+"</td><td>"+booksArr[i].country+"</td><td>"+booksArr[i].language+"</td><td>"+booksArr[i].pages+"</td></tr>"
     }
 }
